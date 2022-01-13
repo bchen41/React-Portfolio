e98f1cda0c3039f0ed7481fcf3676b48add8c958
bchen41 <bettychen41@outlook.com>
2022-01-13 16:45:20 -0500

    added setup for downloading resume link

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -4,6 +4,13 @@ export default function Resume() {
   return (
     <div>
       <h1>Resume</h1>
+      <p>
+        Click{" "}
+        <a href="./assets/bc-resume.pdf" download target="_blank">
+          here
+        </a>{" "}
+        to download resume!
+      </p>
     </div>
   );
 }
