c77fb60bf0cb16387d185fafebdbd3614356d181
bchen41 <bettychen41@outlook.com>
2022-01-14 00:14:41 -0500

    changed displaying of components and rendered pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,39 @@
-import React from "react";
-import PortfolioContainer from "./components/PortfolioContainer";
-// import "./App.css";
+import React, { useState } from "react";
+import Header from "./components/Header";
+import AboutMe from "./components/pages/AboutMe";
+import Portfolio from "./components/pages/Portfolio";
+import ContactMe from "./components/pages/ContactMe";
+import Resume from "./components/pages/Resume";
+import Footer from "./components/Footer";
+import "./App.css";
 
-const App = () => <PortfolioContainer />;
+function App() {
+    const [currentPage, setCurrentPage] = useState("AboutMe");
+
+    const renderPage = () => {
+      if (currentPage === "AboutMe") {
+        return <AboutMe />;
+      }
+      if (currentPage === "Portfolio") {
+        return <Portfolio />;
+      }
+      if (currentPage === "ContactMe") {
+        return <ContactMe />;
+      }
+      if (currentPage === "Resume") {
+        return <Resume />;
+      }
+    };
+  
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+        <Header handlePageChange={handlePageChange} currentPage={currentPage} />
+        {renderPage()}
+        <Footer/>
+        </div>
+    )
+} 
 
 export default App;
