2ee395d272c935a8c265b31c8e9a2240d02c132a
bchen41 <bettychen41@outlook.com>
2022-01-17 02:04:42 -0500

    refactored code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Navigation from "./Navigation"
+import Navigation from "./Navigation";
 
-export default function Header({handlePageChange, currentPage}) {
+export default function Header({ handlePageChange, currentPage }) {
   return (
-    <header className="d-flex justify-content-between">
-      <h1>Betty Chen</h1>
-      <Navigation handlePageChange={handlePageChange} currentPage={currentPage} />
+    <header>
+      <Navigation
+        handlePageChange={handlePageChange}
+        currentPage={currentPage}
+      />
     </header>
   );
 }
