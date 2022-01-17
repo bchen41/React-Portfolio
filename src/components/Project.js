18e3cfbba6e3526aed0b4e74fe768c32665f6b81
bchen41 <bettychen41@outlook.com>
2022-01-17 02:07:20 -0500

    imported fontawesome, refactored a tag to wrap around title&changed css effects

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,17 +1,17 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 export default function Project({ title, demoLink, githubLink, projectImg }) {
   return (
     <React.Fragment>
       <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
         <div className="card-flyer">
-          <div className="text-box">
+          <div className="img-wrap">
             <div className="image-box">
               {demoLink.length > 1 ? (
                 <div>
-                  <a href={demoLink} target="_blank">
-                    <img src={projectImg} alt="Screenshot of BTS Quiz" />
-                  </a>
+                  <img src={projectImg} alt="Screenshot of BTS Quiz" />
                 </div>
               ) : (
                 <a href="javascript:void(0)">
@@ -20,17 +20,21 @@ export default function Project({ title, demoLink, githubLink, projectImg }) {
               )}
             </div>
             {githubLink.length > 1 ? (
-              <div>
-                <div className="text-container">
-                  <h6>{title}</h6>
-                  <p>
-                    Click
-                    <a href={githubLink} target="_blank">
-                      here
-                    </a>
-                    to check out the repository!
-                  </p>
-                </div>
+              <div className="img-description">
+                <a
+                  className="portfolio-title"
+                  href={demoLink}
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  <div>{title}</div>
+                </a>
+
+                <a href={githubLink} target="_blank" rel="noreferrer">
+                  <i className="fab fa-github fa-3x">
+                    <FontAwesomeIcon icon={faGithub} />
+                  </i>
+                </a>
               </div>
             ) : null}
           </div>
