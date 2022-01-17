9abd6da8e60ec865bed53f90a716370ea6c70e3b
bchen41 <bettychen41@outlook.com>
2022-01-17 02:09:16 -0500

    used bootstrap form, personalized msg after submission, input fields are set to required for notifications

--- src/components/pages/ContactMe.js
+++ src/components/pages/ContactMe.js
@@ -1,20 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
+import { Form, Button, Container, Row } from "react-bootstrap";
+import ThanksImg from "../../assets/images/thankyou.png";
 
-export default function ContactMe() {
+const ContactMe = () => {
+  const [submitted, setSubmitted] = useState(false);
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
+  };
+
+  if (submitted) {
+    return (
+      <>
+        <div className="text-center">
+          <h2 className="text-2xl">{`Thank you ${name}!`}</h2>
+          <div className="text-md">
+            {`We'll be in touch soon. Make sure to check your email at ${email} for updates!`}
+          </div>
+          <img src={ThanksImg} width={800}></img>
+        </div>
+      </>
+    );
+  }
+
   return (
-    <div>
-      <h1>Contact Me</h1>
-      <div class="icon-container">
-        <a href="mailto:bettychen41@outlook.com">
-          <i class="fas fa-envelope fa-3x"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/bettychen41/" target="_blank">
-          <i class="fab fa-linkedin-in fa-3x"></i>
-        </a>
-        <a href="https://github.com/bchen41" target="_blank">
-          <i class="fab fa-github fa-3x"></i>
-        </a>
-      </div>
-    </div>
+    <Container>
+      <Row>
+        <Form className="text-center" onSubmit={handleSubmit}>
+          <h1>Contact Me</h1>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Enter Your Name"
+              name="name"
+              required
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Message</Form.Label>
+
+            <Form.Control
+              as="textarea"
+              aria-label="With textarea"
+              type="text"
+              placeholder="Enter Your Message"
+              name="message"
+              className="px-3 py-3 placeholder-gray-400 text-gray-600 relative bg-white bg-white rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring w-full"
+              required
+            />
+            <Form.Text className="text-muted"></Form.Text>
+          </Form.Group>
+
+          <Button type="submit">Submit</Button>
+        </Form>
+      </Row>
+    </Container>
   );
-}
+};
+
+export default ContactMe;
