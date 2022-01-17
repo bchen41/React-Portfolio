a4c8e63ba7ec599af48e4ec9255501bfdf6454be
bchen41 <bettychen41@outlook.com>
2022-01-17 15:50:00 -0500

    added classNames/refactored code for styling, font-family, animation effects

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,35 +5,44 @@ import { faLinkedin, faGithubSquare } from "@fortawesome/free-brands-svg-icons";
 
 export default function Footer() {
   return (
-    <footer className="text-center">
-      <h3>Made with ❤️️ by Betty Chen</h3>
-      <div>
-        <div className="icon-container">
-          <a
-            href="mailto:bettychen41@outlook.com"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-envelope fa-3x">
-              <FontAwesomeIcon icon={faEnvelope} />
-            </i>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/bettychen41/"
-            target="_blank"
-            rel="noreferrer"
-          >
-            <i className="fab fa-linkedin-in fa-3x">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </i>
-          </a>
-          <a href="https://github.com/bchen41" target="_blank" rel="noreferrer">
-            <i className="fab fa-github fa-3x">
-              <FontAwesomeIcon icon={faGithubSquare} />
-            </i>
-          </a>
+    <div className="footer-container">
+      <footer className="text-center">
+        <h5>Made with ❤️️ by Betty Chen</h5>
+        <div>
+          <div className="icon-container">
+            <a
+              href="mailto:bettychen41@outlook.com"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-envelope fa-3x">
+                <FontAwesomeIcon className="footer-icons" icon={faEnvelope} />
+              </i>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/bettychen41/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-linkedin-in fa-3x">
+                <FontAwesomeIcon className="footer-icons" icon={faLinkedin} />
+              </i>
+            </a>
+            <a
+              href="https://github.com/bchen41"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <i className="fab fa-github fa-3x">
+                <FontAwesomeIcon
+                  className="footer-icons"
+                  icon={faGithubSquare}
+                />
+              </i>
+            </a>
+          </div>
         </div>
-      </div>
-    </footer>
+      </footer>
+    </div>
   );
 }
