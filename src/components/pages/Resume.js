9c360194e74737586803bbd88fbf475f71516906
bchen41 <bettychen41@outlook.com>
2022-01-29 21:04:46 -0500

    added logos, refactored tech skills to icons, svg and png

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,9 +1,24 @@
 import React from "react";
 import { Container, Row, Col, Card, ListGroup } from "react-bootstrap";
 import { faDownload } from "@fortawesome/free-solid-svg-icons";
-
+import {
+  faHtml5,
+  faCss3,
+  faJsSquare,
+  faReact,
+  faBootstrap,
+  faGitAlt,
+  faNodeJs,
+} from "@fortawesome/free-brands-svg-icons";
 import BCResume from "../../assets/bc-resume.pdf";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../css/Resume.css";
+import MongoDB from "../../assets/images/mongodb.svg";
+import MongooseODM from "../../assets/images/mongooseODM.png";
+import Express from "../../assets/images/expressjs.svg";
+import MySQL from "../../assets/images/mysql.png";
+import GraphQL from "../../assets/images/graphql.png";
+import JQuery from "../../assets/images/jquery.png";
 export default function Resume() {
   return (
     <Container className="text-center">
@@ -19,17 +34,49 @@ export default function Resume() {
         </p>
         <h3>Proficiencies</h3>
       </Row>
-      <Row>
+      <Row className="logo-card-container">
         <Col>
           <Card>
             <Card.Header>Front-End</Card.Header>
             <ListGroup variant="flush">
-              <ListGroup.Item>HTML/CSS</ListGroup.Item>
-              <ListGroup.Item>JavaScript/jQuery</ListGroup.Item>
-              <ListGroup.Item>Responsive/Mobile Design</ListGroup.Item>
-              <ListGroup.Item>React</ListGroup.Item>
-              <ListGroup.Item>Git/Version Control</ListGroup.Item>
-              <ListGroup.Item>Bootstrap</ListGroup.Item>
+              <ListGroup.Item className="d-flex resume-icon-container">
+                <div className="resume-icons">
+                  <i className="fab fa-html5 fa-3x">
+                    <FontAwesomeIcon icon={faHtml5} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <i className="fab fa-css3 fa-3x">
+                    <FontAwesomeIcon icon={faCss3} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <i className="fab fa-js-square fa-3x">
+                    <FontAwesomeIcon icon={faJsSquare} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <i className="fab fa-react fa-3x">
+                    <FontAwesomeIcon icon={faReact} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <i className="fab fa-bootstrap fa-3x">
+                    <FontAwesomeIcon icon={faBootstrap} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <i className="fab fa-git-alt fa-3x">
+                    <FontAwesomeIcon icon={faGitAlt} />
+                  </i>
+                </div>
+                <div>
+                  <img
+                    src={JQuery}
+                    style={{ width: "65px", paddingTop: "12px" }}
+                  />
+                </div>
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
@@ -37,12 +84,43 @@ export default function Resume() {
           <Card>
             <Card.Header>Back-End</Card.Header>
             <ListGroup variant="flush">
-              <ListGroup.Item>Node</ListGroup.Item>
-              <ListGroup.Item>Express</ListGroup.Item>
-              <ListGroup.Item>RESTful APIs</ListGroup.Item>
-              <ListGroup.Item>MySQL/Sequelize</ListGroup.Item>
-              <ListGroup.Item>MongoDB/Mongoose</ListGroup.Item>
-              <ListGroup.Item>GraphQL</ListGroup.Item>
+              <ListGroup.Item className="d-flex resume-icon-container">
+                <div className="resume-icons">
+                  <i className="fab fa-node-js fa-3x">
+                    <FontAwesomeIcon icon={faNodeJs} />
+                  </i>
+                </div>
+                <div className="resume-icons">
+                  <img
+                    src={MongoDB}
+                    style={{ width: "45px", paddingTop: "15px" }}
+                  />
+                </div>
+                <div>
+                  <img
+                    src={MongooseODM}
+                    style={{ width: "65px", paddingTop: "5px" }}
+                  />
+                </div>
+                <div>
+                  <img
+                    src={Express}
+                    style={{ width: "85px", paddingTop: "7px" }}
+                  />
+                </div>
+                <div>
+                  <img
+                    src={MySQL}
+                    style={{ width: "55px", paddingTop: "9px" }}
+                  />
+                </div>
+                <div className="resume-icons">
+                  <img
+                    src={GraphQL}
+                    style={{ width: "52px", paddingTop: "7px" }}
+                  />
+                </div>
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
